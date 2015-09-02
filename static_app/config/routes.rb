083dfa3e77e4ad610f1cc07a3acd4a2f9cccbe5f
lukemolloy <luke_d_a@hotmail.com>
2015-09-02 19:50:45 +0000

    Prototype

--- static_app/config/routes.rb
+++ static_app/config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  resources :microposts
+  get 'sessions/new'
+
+  get 'users/new'
+
+  get 'users/new'
+
+  get 'users/new'
+
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -27,7 +44,7 @@ Rails.application.routes.draw do
   #       get 'sold'
   #     end
   #   end
-  root 'users#index'
+
   # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
